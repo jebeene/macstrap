1aa9dd078de0549ce6cf50fe75bb3a7d329d2fa8
jebeene <89660618+jebeene@users.noreply.github.com>
2025-11-17 08:31:39 -0500

    fix install path

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -eEo pipefail
 # Define Omarchy locations
 export MACSTRAP_PATH="${HOME}/.local/share/macstrap"
 export MACSTRAP_INSTALL="$MACSTRAP_PATH/install"
-export MACSTRAP_INSTALL_LOG_FILE="/var/log/macstrap-install.log"
+export MACSTRAP_INSTALL_LOG_FILE="$MACSTRAP_PATH/logs/macstrap-install.log"
 export PATH="$MACSTRAP_PATH/bin:$PATH"
 
 # Install
