92021669360c49a1708320c76d7ef5b97f4f1b27
jebeene <89660618+jebeene@users.noreply.github.com>
2025-11-17 22:42:45 -0500

    fix run logged issue with macos

--- install/config/all.sh
+++ install/config/all.sh
@@ -1,3 +1,3 @@
 run_logged $MACSTRAP_INSTALL/config/config.sh
 run_logged $MACSTRAP_INSTALL/config/git.sh
-run_logged $MACSTRAP_INSTALL/config/macos/all.sh
+source $MACSTRAP_INSTALL/config/macos/all.sh
