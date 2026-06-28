30c5d4d575a17a7c7f98547a6c91b042ff1064a3
jebeene <89660618+jebeene@users.noreply.github.com>
2026-06-28 17:37:22 -0400

    merge

--- install/setup/all.sh
+++ install/setup/all.sh
@@ -2,5 +2,3 @@ run_logged $MACSTRAP_INSTALL/setup/bat.sh
 run_logged $MACSTRAP_INSTALL/setup/nvim.sh
 run_logged $MACSTRAP_INSTALL/setup/aerospace.sh
 run_logged $MACSTRAP_INSTALL/setup/tmux.sh
-# run_logged $MACSTRAP_INSTALL/setup/launcher.sh
-
